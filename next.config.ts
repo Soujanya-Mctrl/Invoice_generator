fefcc9b9a83adca8c147fe52627252810e9b8003
Soujanya Mallick <soujanyamallick199@gmail.com>
2025-11-29 20:23:48 +0530

    Configure Turbopack and add initial test for utils

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  turbopack: {},
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
